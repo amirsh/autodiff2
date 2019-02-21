e9e388e8692c62f8578dc16628d9b5106208f64f
Amir Shaikhha <amir.shaikhha@epfl.ch>
2019-02-21 19:39:43 +0000

    BA benchmark for diffsharp.

--- autodiff-exps/diffsharp/diffsharp-exps/Program.fs
+++ autodiff-exps/diffsharp/diffsharp-exps/Program.fs
@@ -29,6 +29,9 @@ let vector_dot_d (v1: D[]) (v2: D[]): D =
         sum <- sum + v1.[i] * v2.[i]
     sum
 
+let vector_norm_sq (vec: D[]): D = 
+    vector_dot_d vec vec
+
 let vector_max_d (vec: D[]): D = 
     vec |> Array.max
 
@@ -40,6 +43,58 @@ let matrix_sum_d (matrix: D[,]): D =
 
 let vector_add_d (v1: _[]) (v2: _[]): _[] =
     Array.map2 (fun a b -> a + b) v1 v2
+
+let vector_sub_d (v1: _[]) (v2: _[]): _[] =
+    Array.map2 (fun a b -> a - b) v1 v2
+
+
+let N_CAM_PARAMS = 11
+let ROT_IDX = 0
+let CENTER_IDX = 3
+let FOCAL_IDX = 6
+let X0_IDX = 7
+let RAD_IDX = 9
+
+let radial_distort (rad_params:D[]) (proj:D[]) =
+    let rsq = vector_norm_sq proj
+    let L = 1. + rad_params.[0] * rsq + rad_params.[1] * rsq * rsq
+    vector_mults_d proj L
+
+let proj (X:D[]) = [| X.[0] / X.[2];X.[1] / X.[2] |]
+
+let cross (a:D[]) (b:D[]) =
+      [|a.[1]*b.[2] - a.[2]*b.[1]; a.[2]*b.[0] - a.[0]*b.[2]; a.[0]*b.[1] - a.[1]*b.[0];|]
+
+let rodrigues_rotate_point (rot:D[]) (X:D[]): D[] =
+    let sqtheta = vector_norm_sq rot
+    let theta = sqrt sqtheta
+    let costheta = cos theta
+    let sintheta = sin theta
+    let theta_inv = D 1. / theta
+
+    let w = vector_mults_d rot theta_inv
+    let w_cross_X = cross w X    
+    let tmp = (vector_dot_d w X) * (D 1. - costheta)
+
+    vector_add_d (vector_add_d (vector_mults_d X costheta) (vector_mults_d w_cross_X sintheta)) (vector_mults_d w tmp)
+
+let project (cam:D[]) (X:D[]) =
+    let rot = cam.[ROT_IDX..(ROT_IDX+2)]
+    let translation = cam.[CENTER_IDX..(CENTER_IDX+2)]
+    let kappa = cam.[RAD_IDX..(RAD_IDX+1)]
+    let principal_point = cam.[X0_IDX..(X0_IDX+1)]
+    let focal_length = cam.[FOCAL_IDX]
+
+    let Xcam = rodrigues_rotate_point rot (vector_sub_d X translation)
+    let distorted = radial_distort kappa (proj Xcam)
+    vector_add_d principal_point (vector_mults_d distorted focal_length)
+
+let projall (n: int) (xs: D[]): D[] = 
+    let cam = xs.[0..10]
+    [| for i = 0 to ((n - 11) / 3 - 1) do  
+        let x = xs.[11 + 3 * i .. (13 + 3 * i)] 
+        yield project cam x
+    |] |> Array.concat
 
 let benchmark_micro (dim: int) (iters: int) = 
     let rng = 42
@@ -62,15 +117,28 @@ let benchmark_micro (dim: int) (iters: int) =
 #if ADD
       let vres = jacobian (fun v -> vector_add_d v vec2) vec1 
       total <- total + (vres |> matrix_sum_d)
-#elif DOT
+#else
+#if DOT
       let vres = grad (fun v -> vector_dot_d v vec2) vec1
       total <- total + (vres |> vector_sum_d)
-#elif MAX
+#else 
+#if MAX
       let vres = grad (vector_max_d) vec1
       total <- total + (vres |> vector_sum_d)
-#else
+#else 
+#if MULTS
       let vres = jacobian (fun v -> vector_mults_d vec1 (v.[0] * v.[0])) [| vec2.[0] |]
       total <- total + (vres.[0, *] |> vector_sum_d)
+#else 
+#if BA
+      let vres = jacobian (projall dim) vec1
+      total <- total + (vres |> matrix_sum_d)
+#else
+      total <- D 42.0
+#endif
+#endif
+#endif
+#endif
 #endif
       
 
