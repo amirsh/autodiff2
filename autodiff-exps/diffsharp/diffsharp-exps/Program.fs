714c08e727289135bae9834b1e2366e0aae5da81
Amir Shaikhha <amir.shaikhha@epfl.ch>
2019-02-21 16:33:19 +0000

    max, dot, mults micro benchmarks added.

--- autodiff-exps/diffsharp/diffsharp-exps/Program.fs
+++ autodiff-exps/diffsharp/diffsharp-exps/Program.fs
@@ -23,10 +23,22 @@ let vector_fill_d (rows: int) (value: double): D[] =
 let vector_sum_d (vec: D[]): D = 
     vec |> Array.sum
 
+let vector_dot_d (v1: D[]) (v2: D[]): D = 
+    let mutable sum = D 0.0
+    for i = 0 to v1.Length - 1 do
+        sum <- sum + v1.[i] * v2.[i]
+    sum
+
+let vector_max_d (vec: D[]): D = 
+    vec |> Array.max
+
+let vector_mults_d (vec: D[]) (s: D): D[] = 
+    vec |> Array.map (fun a -> a * s)
+
 let matrix_sum_d (matrix: D[,]): D = 
     matrix |> Seq.cast<D> |> Seq.sum
 
-let vector_add (v1: _[]) (v2: _[]): _[] =
+let vector_add_d (v1: _[]) (v2: _[]): _[] =
     Array.map2 (fun a b -> a + b) v1 v2
 
 let benchmark_micro (dim: int) (iters: int) = 
@@ -47,9 +59,20 @@ let benchmark_micro (dim: int) (iters: int) =
     for i = 0 to iters - 1 do
       vec1.[0] <- 1.0 / (2.0 + vec1.[0])
       vec2.[1] <- 1.0 / (2.0 + vec2.[1])
-      let vres = jacobian (vector_add vec1) vec2 
-      //total <- total + Array.sum (Array.map2 (+) (Array.map2 (+) vec1 vec2) vec3)
-      total <- vres |> matrix_sum_d
+#if ADD
+      let vres = jacobian (fun v -> vector_add_d v vec2) vec1 
+      total <- total + (vres |> matrix_sum_d)
+#elif DOT
+      let vres = grad (fun v -> vector_dot_d v vec2) vec1
+      total <- total + (vres |> vector_sum_d)
+#elif MAX
+      let vres = grad (vector_max_d) vec1
+      total <- total + (vres |> vector_sum_d)
+#else
+      let vres = jacobian (fun v -> vector_mults_d vec1 (v.[0] * v.[0])) [| vec2.[0] |]
+      total <- total + (vres.[0, *] |> vector_sum_d)
+#endif
+      
 
     let totalv = float total
 
