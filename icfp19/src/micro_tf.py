c14aba3853bc919264a392f279e52f141c54e3e3
Amir Shaikhha <amir.shaikhha@cs.ox.ac.uk>
2019-05-30 02:59:09 +0100

    BA added for tensorflow.

--- icfp19/src/micro_tf.py
+++ icfp19/src/micro_tf.py
@@ -10,52 +10,57 @@ import sys
 import warnings
 import tensorflow as tf
 from tensorflow_forward_ad import forward_gradients
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.ops import tensor_array_ops
+from tensorflow.python.framework import dtypes
+
 
 from tensorflow.python.ops.parallel_for import gradients
 
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
 
-# BA_NCAMPARAMS = 11
-# ROT_IDX = 0
-# C_IDX = 3
-# F_IDX = 6
-# X0_IDX = 7
-# RAD_IDX = 9
+BA_NCAMPARAMS = 11
+ROT_IDX = 0
+C_IDX = 3
+F_IDX = 6
+X0_IDX = 7
+RAD_IDX = 9
 
 
-# def cross(a, b):
-#     return T.as_tensor([
-#         a[1] * b[2] - a[2] * b[1],
-#         a[2] * b[0] - a[0] * b[2],
-#         a[0] * b[1] - a[1] * b[0]])
+def cross(a, b):
+    return tf.convert_to_tensor([
+        a[1] * b[2] - a[2] * b[1],
+        a[2] * b[0] - a[0] * b[2],
+        a[0] * b[1] - a[1] * b[0]])
 
 
-# def rodrigues_rotate_point(rot, X):
-#     sqtheta = T.sum(T.sqr(rot))
-#     theta = T.sqrt(sqtheta)
-#     costheta = T.cos(theta)
-#     sintheta = T.sin(theta)
-#     theta_inverse = 1. / theta
+def rodrigues_rotate_point(rot, X):
+    sqtheta = tf.reduce_sum(tf.square(rot))
+    theta = tf.sqrt(sqtheta)
+    costheta = tf.cos(theta)
+    sintheta = tf.sin(theta)
+    theta_inverse = tf.divide(1., theta)
 
-#     w = theta_inverse * rot
-#     w_cross_X = cross(w, X)
-#     tmp = T.dot(w, X) * (1. - costheta)
+    w = tf.scalar_mul(theta_inverse, rot)
+    w_cross_X = cross(w, X)
+    tmp = tf.tensordot(w, X, 1) * (1. - costheta)
 
-#     return X * costheta + w_cross_X * sintheta + w * tmp
-
-
-# def radial_distort(rad_params, proj):
-#     rsq = T.sum(T.sqr(proj))
-#     L = 1. + rad_params[0] * rsq + rad_params[1] * rsq * rsq
-#     return proj * L
+    return tf.add(tf.scalar_mul(costheta,  X), tf.add(tf.scalar_mul(sintheta, w_cross_X), tf.scalar_mul(tmp, w)))
 
 
-# def project(cam, X):
-#     Xcam = rodrigues_rotate_point(
-#         cam[ROT_IDX:ROT_IDX + 3], X - cam[C_IDX:C_IDX + 3])
-#     distorted = radial_distort(cam[RAD_IDX:RAD_IDX + 2], Xcam[0:2] / Xcam[2])
-#     return distorted * cam[F_IDX] + cam[X0_IDX:X0_IDX + 2]
+def radial_distort(rad_params, proj):
+    rsq = tf.reduce_sum(tf.square(proj))
+    L = 1. + rad_params[0] * rsq + rad_params[1] * rsq * rsq
+    return tf.scalar_mul(L, proj)
+
+
+def project(cam, X):
+    Xcam = rodrigues_rotate_point(
+        cam[ROT_IDX:ROT_IDX + 3], tf.subtract(X, cam[C_IDX:C_IDX + 3]))
+    distorted = radial_distort(cam[RAD_IDX:RAD_IDX + 2], tf.scalar_mul(1 / Xcam[2], Xcam[0:2]))
+    return tf.add(tf.scalar_mul(cam[F_IDX], distorted), cam[X0_IDX:X0_IDX + 2])
 
 # def project_all(n, xs):
 #     cam = xs[0:11]
@@ -67,14 +72,26 @@ if not sys.warnoptions:
 #     res = scanTerm
 #     return res.flatten()
 
-# def project_all2(n, xs, cam):
-#     V1 = xs
-#     V2 = cam
-#     scanTerm, updates = theano.scan(lambda i, c : project(c, V1[(11+i*3):(14+i*3)]),
-#                             outputs_info=None,
-#                             sequences=T.arange(n), non_sequences=[V2])
-#     res = scanTerm
-#     return res.flatten()
+def project_all2(n, xs, cam):
+    V1 = xs
+    V2 = cam
+    # res = tf.map_fn(lambda i: project(V2, V1[(11+i*3):(14+i*3)]), np.arange(n))
+    loop_vars = [
+        array_ops.constant(0, dtypes.int32),
+        tensor_array_ops.TensorArray(V2.dtype, n)
+    ]
+    _, res = control_flow_ops.while_loop(
+        lambda j, _: j < n,
+        lambda j, result: (j + 1,
+                           result.write(j, project(V2, V1[(11+j*3):(14+j*3)]))),
+        loop_vars
+    )
+    return tf.reshape(res.stack(), [-1])
+    # scanTerm, updates = theano.scan(lambda i, c : project(c, V1[(11+i*3):(14+i*3)]),
+    #                         outputs_info=None,
+    #                         sequences=T.arange(n), non_sequences=[V2])
+    # res = scanTerm
+    # return res.flatten()
 
 def micro(prog, dim, iters):
     # M = T.dmatrix('M')
@@ -104,15 +121,10 @@ def micro(prog, dim, iters):
     elif(prog == 'max'):
         term = tf.reduce_max(V1)
         dTerm = tf.gradients(term, V1)[0]
-    # elif(prog == 'ba'):
-    #     n = (dim - 11) / 3
-    #     # term = project_all(n, V1)
-    #     # dTerm = T.jacobian(term, V1)
-    #     # dTermF = theano.function([V1], dTerm)
-    #     term = project_all2(n, V1, V2)
-    #     dTerm = T.jacobian(term, V2)
-    #     dTermF = theano.function([V1, V2], dTerm)
-    #     matrixSum = theano.function([M], T.sum(M))
+    elif(prog == 'ba'):
+        n = (dim - 11) / 3
+        term = project_all2(n, V1, V2)
+        dTerm = gradients.jacobian(term, V2)
     else:
         raise ValueError('prog %s not handled' % prog)
 
@@ -123,9 +135,9 @@ def micro(prog, dim, iters):
     times = []
     total = 0.0
     config = tf.ConfigProto()
-    jit_level = tf.OptimizerOptions.OFF
+    # jit_level = tf.OptimizerOptions.OFF
 	# Turns on XLA JIT compilation.
-    # jit_level = tf.OptimizerOptions.ON_1
+    jit_level = tf.OptimizerOptions.ON_1
     # jit_level = tf.OptimizerOptions.ON_2
 
     config.graph_options.optimizer_options.global_jit_level = jit_level
@@ -152,8 +164,9 @@ def micro(prog, dim, iters):
         	res = sess.run(dTerm,
                  feed_dict={V1: v1, V2: v2[1:2]})
         elif(prog == 'ba'):
-            # res = dTermF(v1)
-            res = dTermF(v1, v1[0:11])
+            res = sess.run(dTerm,
+                 feed_dict={V1: v1, V2: v1[0:11]})
+            # res = dTermF(v1, v1[0:11])
         # total += sess.run(tf.reduce_sum(res))
         current_sum = res.sum()
         # print(res)
@@ -168,6 +181,8 @@ def micro(prog, dim, iters):
           total = 0.0
         else:
         	times.append(stats.total_tt)
+
+    sess.close()
     return (total, times)
         
 
